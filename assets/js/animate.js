d94b304d36db72b34e0f60fe7d23818bdf107fbb
Vikas Sanwal <vikassnwl@gmail.com>
2025-04-14 11:49:43 +0530

    Update profile

--- assets/js/animate.js
+++ assets/js/animate.js
@@ -41,7 +41,7 @@ document.querySelectorAll(".menuitem").forEach(function (item) {
     // redirecting to resume on click resume link in menu items
     if (this.dataset.id === "resume") {
       const resume_link =
-        "https://drive.google.com/file/d/1i9-vymrSCdUcxuo7fI0FkqQ2nJhbxjIT/view?usp=sharing";
+        "https://drive.google.com/file/d/1lTx2XX1XqoQKo65VO0VwXgrq4a7_F5Sc/view?usp=sharing";
       window.open(resume_link, "_blank");
     }
     // scrolling to content on click menu items
